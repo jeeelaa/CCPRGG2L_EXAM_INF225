6a8d2c424a7e2179d76d016deec8f5744d210e79
Anjiela Vejar <120693848+jeeelaa@users.noreply.github.com>
2023-02-01 01:12:12 +0800

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,41 +1,34 @@
-package exam1;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-// I HAVE USED ECLIPSE AS A COMPILER SINCE IM HAVING TROUBLE RUNNING MY CODES IN VISUAL STUDIO CODE, PLEASE BEAR WITH ME 
 public class Recursion {
 
 
 	    public static void main(String[] args) {
 	        
-	    	System.out.println("Input Name: ");
-	    	 String fullName = "anjiela vejar";
+	    	
+	    String fullName = "anjiela vejar";
 
 	         removeLetter(fullName);
-	     }
+	    }
 
 	     static void removeLetter(String str) {
-	    	 Scanner sc = new Scanner(System.in);
-		      String input = sc.nextLine();
+	    	 
+	    	 String fullName = "anjiela vejar";
+	    	 
 		      String regex = "([^b/c/d/f/g/h/j/k/l/m/n/p/q/r/s/t/v/w/x/z])";
-		      String constants = "";
 		      
-		      Pattern pattern = Pattern.compile(regex);
 		      
-		      Matcher matcher = pattern.matcher(input);
+		      Pattern figure = Pattern.compile(regex);
+		      
+		      Matcher match = figure.matcher(fullName);
 		      
 		      StringBuffer sb = new StringBuffer();
 		      
-		      while (matcher.find()) {
-		         matcher.appendReplacement(sb, "");
+		      while (match.find()) {
+		         match.appendReplacement(sb, "");
 		      }
-		      matcher.appendTail(sb);
+		      match.appendTail(sb);
 		      System.out.println("Output Name :"+ sb.toString() );
-	    	
-	   
-	    	 
-	    	
+	    
 	    }
 	}
-
